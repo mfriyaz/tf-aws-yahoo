f458229a92a5c46ac1286b46f7605876e2ba708b
Riyaz <mfriyaz@gmail.com>
2025-05-22 19:00:01 +0800

    commit 04

--- 01-one-webserver/main.tf
+++ 01-one-webserver/main.tf
@@ -1,3 +1,15 @@
+# Create a Security Group for an EC2 instance
+resource "aws_security_group" "demosg" {
+	name = "terraform-example-instance-sg
+	
+	ingress {
+		from_port =8080
+		to_port = 8080
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+}
+
 # Create an EC2 instance
 resource "aws_instance" "example" {
   ami           = "ami-0afc7fe9be84307e4"
@@ -7,3 +19,4 @@ tags={
 	Name = "terraform-example"
     }
 }
+
