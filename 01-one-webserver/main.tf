5a47e6aebc3bae432d614ccc433be60c3cb5698e
Riyaz <mfriyaz@gmail.com>
2025-05-22 17:03:56 +0800

    commit 01

---
+++ 01-one-webserver/main.tf
@@ -0,0 +1,5 @@
+# Create an EC2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-0afc7fe9be84307e4"
+  instance_type = "t2.micro"
+}
---
+++ 01-one-webserver/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-1"
+  }
