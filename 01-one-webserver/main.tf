24adfe5e63447e75e0c0201a55e5b931810af74e
Riyaz <mfriyaz@gmail.com>
2025-05-25 19:45:40 +0800

    commit 07

--- 01-one-webserver/main.tf
+++ 01-one-webserver/main.tf
@@ -1,26 +1,56 @@
-# Create a Security Group for an EC2 instance
-resource "aws_security_group" "demosg" {
-	name = "terraform-example-instance-sg"
-	vpc_id = data.aws_vpc.default.id
-	ingress {
-		from_port =22
-		to_port = 22
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-}
-
-# Create an EC2 instance
-resource "aws_instance" "example" {
-  ami           = "ami-0afc7fe9be84307e4"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [ aws_security_group.demosg.id ]
-
-tags={
-	Name = "terraform-example"
-    }
-}
-
+# Get the default VPC
 data "aws_vpc" "default" {
-default = true
+  default = true
+}
+
+# Create security group allowing SSH and HTTP
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Launch Ubuntu EC2 instance
+resource "aws_instance" "ubuntu_instance" {
+  ami                    = "ami-01938df366ac2d954"  # Ubuntu 22.04 LTS for ap-south-east
+  instance_type          = "t2.micro"
+  key_name               = "terraform.pem"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y apache2
+              systemctl enable apache2
+              systemctl start apache2
+              echo "<h1>Ubuntu Web Server Running</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "UbuntuSingleTier"
+  }
 }
