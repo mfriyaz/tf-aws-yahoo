7822cf6b873c4c3426adf8a6a03b26433615215e
Riyaz <mfriyaz@gmail.com>
2025-05-26 11:16:40 +0800

    commit 09

--- 01-one-webserver/main.tf
+++ 01-one-webserver/main.tf
@@ -19,8 +19,8 @@ resource "aws_security_group" "allow_ssh_http" {
 
   ingress {
     description = "HTTP"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
