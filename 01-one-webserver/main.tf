7f6bfc06a4ec6174006b1a928435613672bc664b
Riyaz <mfriyaz@gmail.com>
2025-05-29 18:03:41 +0800

    Public Subnet Creation with Tags

--- 01-one-webserver/main.tf
+++ 01-one-webserver/main.tf
@@ -20,5 +20,10 @@ resource "aws_internet_gateway" "gw" {
 resource "aws_subnet" "public" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
-  availability_zone = "ap_southeast-1"
+  availability_zone = "ap_southeast-1a"
+
+  tags = {
+    Name = "main"
+    Enviromnet = "dev"
+  }
   }
