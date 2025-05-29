0f68d9f317e841e3235d6ec1a0cddbc098024976
Riyaz <mfriyaz@gmail.com>
2025-05-29 19:30:07 +0800

    Private Subnet Creation

--- 01-one-webserver/main.tf
+++ 01-one-webserver/main.tf
@@ -20,10 +20,17 @@ resource "aws_internet_gateway" "gw" {
 resource "aws_subnet" "public" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
   availability_zone = "ap-southeast-1a"
 
   tags = {
     Name = "main"
     Enviromnet = "dev"
   }
+  }
+
+  resource "aws_subnet" "private" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "ap-southeast-1b"
   }
