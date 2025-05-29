488350c82554e6b3da571668171c8ff9af1724d1
Riyaz <mfriyaz@gmail.com>
2025-05-29 17:35:03 +0800

    practice 01

--- 01-one-webserver/main.tf
+++ 01-one-webserver/main.tf
@@ -1,56 +1,7 @@
-# Get the default VPC
-data "aws_vpc" "default" {
-  default = true
-}
-
-# Create security group allowing SSH and HTTP
-resource "aws_security_group" "allow_ssh_http" {
-  name        = "allow_ssh_http"
-  description = "Allow SSH and HTTP inbound traffic"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
   }
 
-  ingress {
-    description = "HTTP"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Launch Ubuntu EC2 instance
-resource "aws_instance" "ubuntu_instance" {
-  ami                    = "ami-01938df366ac2d954"  # Ubuntu 22.04 LTS for ap-south-east
-  instance_type          = "t2.micro"
-  key_name               = "terrafrom"
-  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
-  associate_public_ip_address = true
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install -y apache2
-              systemctl enable apache2
-              systemctl start apache2
-              echo "<h1>Ubuntu Web Server Running</h1>" > /var/www/html/index.html
-              EOF
-
-  tags = {
-    Name = "UbuntuSingleTier"
-  }
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id  
 }
