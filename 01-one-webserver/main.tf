ef58fd5c1f1a051462343e3782e8feede6a2b2dd
Riyaz <mfriyaz@gmail.com>
2025-05-31 12:45:18 +0800

    Ubuntu update

--- 01-one-webserver/main.tf
+++ 01-one-webserver/main.tf
@@ -1,3 +1,5 @@
+
+/*
 resource "aws_vpc" "main" {
     cidr_block = "10.0.0.0/16"
   
@@ -33,4 +35,65 @@ resource "aws_subnet" "public" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.1.0/24"
     availability_zone = "ap-southeast-1b"
+  } */
+
+  provider "aws" {
+  region = "us-east-1" # Change this to your preferred region
+}
+
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Create security group allowing SSH and HTTP
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Launch Ubuntu EC2 instance
+resource "aws_instance" "ubuntu_instance" {
+  ami                    = "ami-0fc5d935ebf8bc3bc"  # Ubuntu 22.04 LTS for us-east-1
+  instance_type          = "t2.micro"
+  key_name               = "terraform.pem"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y apache2
+              systemctl enable apache2
+              systemctl start apache2
+              echo "<h1>Ubuntu Web Server Running</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "UbuntuSingleTier"
+  }
+}
