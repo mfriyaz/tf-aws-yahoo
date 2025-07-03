e0331eb3358672f8457b24c695a7b8bc3ab51b75
Riyaz <mfriyaz@gmail.com>
2025-07-03 18:18:44 +0800

    Commented code remove and update

--- 01-one-webserver/main.tf
+++ 01-one-webserver/main.tf
@@ -1,43 +1,4 @@
 
-/*
-resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
-  
-
-  tags = {
-    Name = "main"
-    Environment = "dev"
-    Owner = "Riyaz"
-    }
-  }
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id  
-  tags = {
-    Name = "main"
-    Environment = "dev"
-  }
-}
-
-resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-  map_public_ip_on_launch = true
-  availability_zone = "ap-southeast-1a"
-
-  tags = {
-    Name = "main"
-    Enviromnet = "dev"
-  }
-  }
-
-  resource "aws_subnet" "private" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "ap-southeast-1b"
-  } */
-
-  
 # Get the default VPC
 data "aws_vpc" "default" {
   default = true
