2057956781cd41d4e8a5e7e675b0e8875fd2b02b
Riyaz <mfriyaz@gmail.com>
2025-08-08 19:59:19 +0800

    Ubuntu Launch

--- 01-one-webserver/main.tf
+++ 01-one-webserver/main.tf
@@ -42,7 +42,7 @@ resource "aws_security_group" "allow_ssh_http" {
 }
 # Launch Ubuntu EC2 instance
 resource "aws_instance" "ubuntu_instance" {
-  ami                    = "ami-0c1907b6d738188e5"  # Ubuntu 22.04 LTS for us-east-1
+  ami                    = "ami-02c7683e4ca3ebf58"  # Ubuntu 22.04 LTS for us-east-1
   instance_type          = "t2.micro"
   key_name               = "terrafrom"
   vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
